076fda49d9638b5713e692866600b4828dfeba14
Łukasz Krawczyk <lukaszkrawczyklodz@gmail.com>
2018-04-01 20:55:24 +0200

    Add PHPunit.

---
+++ Tests/BasicTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use \PHPUnit\Framework\TestCase;
+
+final class BasicTest extends TestCase
+{
+    public function testCanBeCreatedFromValidEmailAddress(): void
+    {
+        $this->assertEquals(1,1);
+    }
+}
